10f069599b7d69882c4bd0d782ce1c1309f19640
danconst <98926395+danconst@users.noreply.github.com>
2023-03-19 19:15:05 -0400

    attempted fix of the "date" issue

--- client/src/views/Log.vue
+++ client/src/views/Log.vue
@@ -1,43 +1,9 @@
-<template>
-    <div>
-      <WorkoutForm @submit="addWorkout" />
-      <div v-if="workouts.length > 0">
-        <h2 class="title is-2 has-text-centered">Workout Log</h2>
-        <table class="table is-striped is-centered">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Type</th>
-              <th>Distance (km)</th>
-              <th>Duration (min)</th>
-              <th>Pace (min/km)</th>
-              <th>Calories Burnt</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(workout, index) in workouts" :key="index">
-              <td>{{ workout.date }}</td>
-              <td>{{ workout.workoutType }}</td>
-              <td>{{ workout.distance }}</td>
-              <td>{{ workout.duration }}</td>
-              <td>{{ workout.pace }}</td>
-              <td>{{ workout.calories }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div v-else>
-        <p>No workouts logged yet.</p>
-      </div>
-    </div>
-  </template>
   
   <script lang="ts">
   import { defineComponent } from 'vue';
   import WorkoutForm from '../components/WorkoutForm.vue';
   
   interface Workout {
-    date: Date;
     workoutType: string;
     distance: number;
     duration: number;
@@ -57,7 +23,6 @@
     methods: {
       addWorkout(workout: Workout) {
         this.workouts.push({
-          date: new Date(),
           ...workout,
         });
       },
@@ -65,6 +30,42 @@
   });
   </script>
   
+
+
+<template>
+    <div>
+      <WorkoutForm @submit="addWorkout" />
+      <div v-if="workouts.length > 0">
+        <h2 class="title is-2 has-text-centered">Workout Log</h2>
+        <table class="table is-striped is-centered">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Type</th>
+              <th>Distance (km)</th>
+              <th>Duration (min)</th>
+              <th>Pace (min/km)</th>
+              <th>Calories Burnt</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+            <tr v-for="(workout, index) in workouts" :key="index">
+              <td>{{ new Date()}}</td>
+              <td>{{ workout.workoutType }}</td>
+              <td>{{ workout.distance }}</td>
+              <td>{{ workout.duration }}</td>
+              <td>{{ workout.pace }}</td>
+              <td>{{ workout.calories }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-else>
+        <p>No workouts logged yet.</p>
+      </div>
+    </div>
+  </template>
   <style scoped>
   table {
     margin-top: 15px;
