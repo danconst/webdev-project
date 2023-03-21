97096ed7747e1aae28d63887acf9d75b13ca0a2f
danconst <98926395+danconst@users.noreply.github.com>
2023-03-20 22:16:14 -0400

    WOOOOO!!!!! NOW WE'RE COOKING!!!!

--- client/src/model/workouts.ts
+++ client/src/model/workouts.ts
@@ -1,5 +1,8 @@
 import { ref } from 'vue';
 import { useSession } from './session';
+import userWorkout from '../data/workouts.json';
+import usersData from '../data/users.json';
+
 export interface Workout {
   user: string;
   date: Date;
@@ -11,7 +14,50 @@ export interface Workout {
 }
 
 const workouts = ref<Workout[]>([]);
-const session =  useSession();
+const session = useSession();
+
+
+function updateUserStats(workout: Workout) {
+  const user = session.user;
+  if (!user) {
+    return;
+  }
+  user.dDist = (user.dDist || 0) + workout.distance;
+  user.dDur = (user.dDur || 0) + workout.duration;
+  user.dPace = ((user.dPace || 0) * (workouts.value.length - 1) + workout.pace) / workouts.value.length;
+  user.dCal = (user.dCal || 0) + workout.calories;
+
+  user.wDist = (user.wDist || 0) + workout.distance;
+  user.wDur = (user.wDur || 0) + workout.duration;
+  user.wPace = ((user.wPace || 0) * (workouts.value.length - 1) + workout.pace) / workouts.value.length;
+  user.wCal = (user.wCal || 0) + workout.calories;
+
+  user.aDist = (user.aDist || 0) + workout.distance;
+  user.aDur = (user.aDur || 0) + workout.duration;
+  user.aPace = ((user.aPace || 0) * (workouts.value.length - 1) + workout.pace) / workouts.value.length;
+  user.aCal = (user.aCal || 0) + workout.calories;
+}
+
+for (const user of usersData.users) {
+  user.dDist = 0;
+  user.dDur = 0;
+  user.dPace = 0;
+  user.dCal = 0;
+}
+
+
+
+for (const workout of userWorkout.workouts) {
+  workouts.value.push({
+    user: workout.user,
+    date: new Date(workout.date),
+    workoutType: workout.workoutType,
+    distance: parseFloat(workout.distance),
+    duration: parseFloat(workout.duration),
+    pace: parseFloat(workout.pace),
+    calories: parseInt(workout.calories),
+  });
+}
 
 export function addWorkout(workout: Workout) {
   workouts.value.push({
@@ -19,6 +65,7 @@ export function addWorkout(workout: Workout) {
     user: session.user?.name || "Unknown user",
     date: new Date(),
   });
+  updateUserStats(workout);
 }
 
 export function getWorkouts() {
